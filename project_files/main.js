680af44381866a3fa63d86466275b83727342475
wasif zaman <wasif.zaman1@students.mq.edu.au>
2023-11-30 22:33:26 +1100

    quote generator

---
+++ project_files/main.js
@@ -0,0 +1,23 @@
+var quotes = [
+
+    "Don't watch the clock; do what it does. Keep going.",
+    "Success is not the key to happiness. Happiness is the key to success.",
+    "Believe you can and you're halfway there.",
+    "Duniya mein aise log kaite hai, itne cheez ke aise pau ke saari duniya itni tumari paa",
+    "grab my but will u",
+    "Some people are worth melting for Aarondale",
+    "Would rather sweat in practice or bleed in battle - Aaron",
+    "I am the one who knocks",
+    "I am the danger",
+    "I am batman",
+    "I am vengeance",
+    "Comparison is the theif of joy",
+    "I am the captian now",
+    "all iz well",
+    "what will you have after 500 years",
+    "builti multitool",
+    
+
+
+
+]; // Replace with your quotes
